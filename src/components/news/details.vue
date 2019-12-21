14039f65603fb539a09b9770551368a5c775d1b2
L-Vanilla <1135063038@qq.com>
2019-12-21 13:26:28 +0800

    新闻管理页面微调

---
+++ src/components/news/details.vue
@@ -0,0 +1,63 @@
+<template>
+  <div style="margin-top: 15px;">
+    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+      <el-form-item label="新闻标题" prop="title">
+        <el-input v-model="ruleForm.title"  style="width:300%" disabled></el-input>
+      </el-form-item>
+      <el-form-item label="内容" prop="content">
+        <el-input  type="textarea" :autosize="{ minRows: 5, maxRows: 10}" style="width:300%"  v-model="ruleForm.content" disabled></el-input>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+  export default {
+    inject:['reload'],
+    props:["id"],
+    data () {
+      name:"details"
+      return {
+        ruleForm:{
+          id:"",
+          title:"",
+          content:""
+        },
+        buttonText:"创建"
+
+      }
+    },
+    created(){
+      if(this.id){
+        this.get("news/getOne",(data)=>{
+          this.ruleForm=data;
+          console.log(this.ruleForm);
+        },{id:this.id});
+        this.buttonText="修改"
+      }
+
+    },
+    components: {
+
+    },
+    methods:{
+      // resetForm(formName){
+      //   this.$refs[formName].resetFields();
+      // },
+      // submitForm(formName){
+      //   let url="";
+      //   if(this.id)
+      //     url="news/update";
+      //   else
+      //     url="news/add";
+      //   this.post(formName,url,this.ruleForm);
+      // }
+
+    }
+  }
+
+</script>
+
+<style scoped>
+
+</style>
